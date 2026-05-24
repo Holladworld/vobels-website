7d4ab5586c4c4c16e519101669bd72259a0f21d3
Holladworld <oladimejiolayinka95@gmail.com>
2026-05-24 18:07:15 +0300

    Fix Cloudflare issue

--- src/pages/api/flipbook.ts
+++ src/pages/api/flipbook.ts
@@ -476,6 +476,7 @@ export const OPTIONS: APIRoute = async () => {
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '86400'
         }
